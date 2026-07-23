284fb26022e04f546ef5360360ad2d374a887e73
skfahad <skfahadco@gmaol.com>
2026-07-24 00:49:47 +0530

    Added endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\HealthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api/health', [HealthController::class, 'health']);
